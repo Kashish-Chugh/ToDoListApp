027fe4e3e1053687913b8150ce1a687438f390f4
Kashish-Chugh <chughkashish12@gmail.com>
2020-03-30 20:35:52 +0530

    add gitignore,Procfile

--- app.js
+++ app.js
@@ -150,6 +150,11 @@ app.get("/:category", (req, res) => {
     });
 });
 
-app.listen(3000, (req, res) => {
-    console.log("server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, (req, res) => {
+    console.log("server started successfully");
 });
